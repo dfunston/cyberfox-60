ce8977c6e4dec546baaad9236a0cc90c37d4b5b2
Daniel Funston <daniel.funston@gmail.com>
2024-07-27 12:27:23 -0500

    More overhaul. TODO: Need to renet the pins.

--- footprints/EVQWGD001.js
+++ footprints/EVQWGD001.js
@@ -38,8 +38,7 @@ module.exports = {
       const standard = `
         (module RollerEncoder_Panasonic_EVQWGD001 (layer F.Cu) (tedit 6040A10C)
         ${p.at /* parametric position */}   
-        (fp_text reference REF** (at 0 0 ${p.r}) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
-        (fp_text value EVQWGD001 (at -0.1 9 ${p.r}) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
+        (fp_text value EVQWGD001 (at 0 0 ${p.r}) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
         
         ${'' /* corner marks */}
         (fp_line (start -8.4 -6.4) (end 8.4 -6.4) (layer Dwgs.User) (width 0.12))
@@ -50,14 +49,14 @@ module.exports = {
       function pins(def_neg, def_pos) {
         return `
           ${'' /* edge cuts */}
-          (fp_line (start ${def_pos}9.8 7.3) (end ${def_pos}9.8 -6.3) (layer Edge.Cuts) (width 0.15))
           (fp_line (start ${def_pos}7.4 -6.3) (end ${def_pos}7.4 7.3) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}7.4 -6.3) (mid ${def_pos}7.487868 -6.512132) (end ${def_pos}7.7 -6.6) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
           (fp_line (start ${def_pos}9.5 -6.6) (end ${def_pos}7.7 -6.6) (layer Edge.Cuts) (width 0.15))
-          (fp_line (start ${def_pos}7.7 7.6) (end ${def_pos}9.5 7.6) (layer Edge.Cuts) (width 0.15))
-          (fp_arc (start ${def_pos}7.7 7.3) (mid ${def_pos}7.487868 7.512132) (end ${def_pos}7.4 7.3) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}9.5 -6.6) (mid ${def_pos}9.712132 -6.512132) (end ${def_pos}9.8 -6.3) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+          (fp_line (start ${def_pos}9.8 7.3) (end ${def_pos}9.8 -6.3) (layer Edge.Cuts) (width 0.15))
           (fp_arc (start ${def_pos}9.8 7.3) (mid ${def_pos}9.712132 7.512132) (end ${def_pos}9.5 7.6) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
-          (fp_arc (start ${def_pos}7.7 -6.3) (mid ${def_pos}7.487868 -6.512132) (end ${def_pos}7.7 -6.6) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
-          (fp_arc (start ${def_pos}9.5 -6.3) (mid ${def_pos}9.712132 -6.512132) (end ${def_pos}9.8 -6.3) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+          (fp_line (start ${def_pos}7.7 7.6) (end ${def_pos}9.5 7.6) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}7.7 7.6) (mid ${def_pos}7.487868 7.512132) (end ${def_pos}7.4 7.3) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
 
           ${'' /* pins */}
           (pad S1 thru_hole circle (at ${def_neg}6.85 -6.2 ${p.r}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.from})
